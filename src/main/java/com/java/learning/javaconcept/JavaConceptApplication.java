792a5492f270e2d1cf1e7b0cdc8ce77722429682
Ankit Kumar <105793993+ankitkumaro632@users.noreply.github.com>
2024-04-12 16:00:57 +0530

    Initial commit

---
+++ src/main/java/com/java/learning/javaconcept/JavaConceptApplication.java
@@ -0,0 +1,13 @@
+package com.java.learning.javaconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConceptApplication.class, args);
+	}
+
+}
